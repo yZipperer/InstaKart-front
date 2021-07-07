61d8fc984294ada5476095e7e34ee9a347bc4dfd
unknown <yz768737@gmail.com>
2021-07-06 19:38:59 -0600

    Updates to the menu and style

--- src/components/menu/Nav.js
+++ src/components/menu/Nav.js
@@ -37,38 +37,35 @@ const Nav = () => {
                   </div>
 
                   <div class="absolute right-0 flex items-center pr-2 sm:ml-6 sm:pr-0">
-                      <button class="p-1 h-8 w-8 rounded-full text-white hover:bg-gray-500 hidden hover:text-white sm:inline-block">
+                      <button class="p-1 h-8 w-8 rounded-full text-white hover:bg-blue-400 hidden hover:text-white sm:inline-block">
                         <i class="far fa-bell"></i>
                       </button>
 
                       <div class="ml-3 relative">
                           <div class="dropdown">
-                              <div class="flex text-sm">
-                                <img class="h-8 w-8 rounded-full" src="./images/avatar.jpg" alt="Profile Image" />
-                                <span class="text-gray-400 flex items-center justify-center h-8 pl-4 pr-4 ml-2 rounded-full hover:text-white hover:bg-gray-500">Admin</span>
-                              </div>
+                            <div class="flex text-sm">
+                              <span class="text-white flex items-center justify-center h-8 pl-4 pr-4 ml-2 rounded-full hover:text-white hover:bg-blue-400">Admin</span>
+                            </div>
 
-                            <ul className="dropdown-menu absolute hidden text-gray-200 pt-1">
-                                <li className="">
-                                  <Link className="bg-gray-200 hover:bg-gray-300 py-2 px-5 text-black block whitespace-no-wrap">Profile</Link>
-                                </li>
-                                <li className="">                                  
-                                  <a href="#" onClick={logout} class="bg-gray-200 hover:bg-gray-300 text-black py-2 px-5 block whitespace-no-wrap">Sign out</a>
-                                </li>
+                            <ul className="dropdown-menu absolute hidden text-gray-300 pt-1">
+                              <li className="">
+                                <Link className="bg-gray-100 hover:bg-gray-200 py-2 px-5 text-black block whitespace-no-wrap">Profile</Link>
+                              </li>
+                              <li className="">                                  
+                                <a href="#" onClick={logout} class="bg-gray-100 hover:bg-gray-200 text-black py-2 px-5 block whitespace-no-wrap">Sign out</a>
+                              </li>
                             </ul>
                           </div>
 
-                          
-                          
                       </div>
                   </div>
                 </div>
                 {mobileMenuOpen ? (
-                  <div class="sm:hidden" id="mobileMenu">
-                    <div class="px-2 pt-2 pb-3 space-y-1">
-                      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-                      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-                      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                  <div class="sm:hidden bg-blue-400 w-full" id="mobileMenu">
+                    <div class="mt-2 mb-3  space-y-1">
+                      <a href="#" class="text-white hover:bg-blue-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
+                      <a href="#" class="text-white hover:bg-blue-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">New and Trending</a>
+                      <a href="#" class="text-white hover:bg-blue-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Deals</a>
                     </div>
                   </div>
                 ) : (
