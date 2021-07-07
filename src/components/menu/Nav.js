7a0de58f950dafbdfcb6fea0b9c5d63856801f7f
unknown <yz768737@gmail.com>
2021-07-06 19:20:43 -0600

    Added Toggling Menus
    I have now made the profile menu toggle when it is hovered over. This is much better than the previous one which required you to click to toggle it and then have to click it again just to turn it off. Also, the mobile menu now toggles as well.

--- src/components/menu/Nav.js
+++ src/components/menu/Nav.js
@@ -4,7 +4,7 @@ import firebase from 'firebase';
 import {useDispatch} from 'react-redux';
 
 const Nav = () => {
-  const [profileMenuOpen, setProfileMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   let dispatch = useDispatch();
   let history = useHistory();
@@ -25,7 +25,7 @@ const Nav = () => {
           <nav class="bg-blue-500 mx-auto px-2 sm:px-6 lg:px-8">
                 <div class="relative flex items-center justify-between h-16">
                   <div class="absolute flex items-center sm:hidden">
-                    <button type="button" id="mobileMenuButton" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700">
+                    <button type="button" id="mobileMenuButton" class="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-blue-400" onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
                       <i class="fa fa-bars" aria-hidden="true"></i>
                     </button>
                   </div>
@@ -37,43 +37,44 @@ const Nav = () => {
                   </div>
 
                   <div class="absolute right-0 flex items-center pr-2 sm:ml-6 sm:pr-0">
-                      <button class="p-1 h-8 w-8 rounded-full text-gray-400 hover:bg-gray-500 hidden hover:text-white sm:inline-block">
+                      <button class="p-1 h-8 w-8 rounded-full text-white hover:bg-gray-500 hidden hover:text-white sm:inline-block">
                         <i class="far fa-bell"></i>
                       </button>
 
                       <div class="ml-3 relative">
-                          <div>
-                              <button type="button" class="flex text-sm" id="profileMenuButton" onClick={() => setProfileMenuOpen(!profileMenuOpen)}>
-                                  <img class="h-8 w-8 rounded-full" src="./images/avatar.jpg" alt="Profile Image" />
-                                  <span class="text-gray-400 flex items-center justify-center h-8 pl-4 pr-4 ml-2 rounded-full hover:text-white hover:bg-gray-500">Admin</span>
-                              </button>
+                          <div class="dropdown">
+                              <div class="flex text-sm">
+                                <img class="h-8 w-8 rounded-full" src="./images/avatar.jpg" alt="Profile Image" />
+                                <span class="text-gray-400 flex items-center justify-center h-8 pl-4 pr-4 ml-2 rounded-full hover:text-white hover:bg-gray-500">Admin</span>
+                              </div>
+
+                            <ul className="dropdown-menu absolute hidden text-gray-200 pt-1">
+                                <li className="">
+                                  <Link className="bg-gray-200 hover:bg-gray-300 py-2 px-5 text-black block whitespace-no-wrap">Profile</Link>
+                                </li>
+                                <li className="">                                  
+                                  <a href="#" onClick={logout} class="bg-gray-200 hover:bg-gray-300 text-black py-2 px-5 block whitespace-no-wrap">Sign out</a>
+                                </li>
+                            </ul>
                           </div>
 
-                          {profileMenuOpen ? (
-                            <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" id="profileMenu">
-                              <a href="#" class="block px-4 py-2 text-sm text-gray-700">Profile</a>
-                              <a href="#" class="block px-4 py-2 text-sm text-gray-700">Settings</a>
-                              <a href="#" onClick={logout} class="block px-4 py-2 text-sm text-gray-700">Sign out</a>
-                          </div>
-                          ) : (
-                            <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" hidden id="profileMenu">
-                              <a href="#" class="block px-4 py-2 text-sm text-gray-700">Profile</a>
-                              <a href="#" class="block px-4 py-2 text-sm text-gray-700">Settings</a>
-                              <a href="#" onClick={logout} class="block px-4 py-2 text-sm text-gray-700">Sign out</a>
-                            </div>
-                          )}
+                          
                           
                       </div>
                   </div>
                 </div>
-            
-              <div class="sm:hidden" hidden id="mobileMenu">
-                <div class="px-2 pt-2 pb-3 space-y-1">
-                  <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-                  <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-                  <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
-                </div>
-              </div>
+                {mobileMenuOpen ? (
+                  <div class="sm:hidden" id="mobileMenu">
+                    <div class="px-2 pt-2 pb-3 space-y-1">
+                      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+                      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+                      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                    </div>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              
             </nav>
       </>
     );
