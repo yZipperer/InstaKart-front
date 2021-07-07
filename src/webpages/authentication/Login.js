5a1bd000d1373073308f95afe5b55ba382147612
unknown <yz768737@gmail.com>
2021-07-07 11:38:05 -0600

    Added Google Log In
    I have added google login. I also added a notification whenever you login to know that the login was successful.

--- src/webpages/authentication/Login.js
+++ src/webpages/authentication/Login.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-import {auth} from '../../firebase';
+import {auth, googleAuthProvider} from '../../firebase';
 import {useDispatch} from 'react-redux';
 import {toast} from 'react-toastify';
 
@@ -30,12 +30,45 @@ const Login = ({history}) => {
             });
             setLoading(false);
 
+            toast.success("Log In was successful", {
+                position: "top-center",
+                draggable: true
+            });
+
             history.push('/');
         } catch (err) {
             console.log(err);
             toast.error(err.message);
             setLoading(false);
         }
+    };
+
+    const loginWithGoogle = async () => {
+        auth.signInWithPopup(googleAuthProvider)
+        .then(async (result) => {
+            const {user} = result;
+            const tokenResult = await user.getIdTokenResult();
+
+            dispatch({
+                type: "LOGGED_IN",
+                payload: {
+                  name: "name",
+                  email: user.email,
+                  token: tokenResult.token
+                }
+            });
+
+            toast.success("Log In was successful", {
+                position: "top-center",
+                draggable: true
+            });
+
+            history.push('/');
+        })
+        .catch(err => {
+            console.log(err);
+            toast.error(err.message);
+        });
     };
 
     const loginForm = () => {
@@ -69,9 +102,15 @@ const Login = ({history}) => {
         <div style={{height: "94.1vh"}} className="bg-gray-200 flex flex-col">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center pb-24 justify-center px-2">
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    {loading ? <h1 className="mb-8 text-3xl text-center">Log In</h1> : <h1 className="mb-8 text-3xl text-center">Loading...</h1>}
+                    {loading ? <h1 className="mb-8 text-3xl text-center">Loading...</h1> : <h1 className="mb-8 text-3xl text-center">Log In</h1>}
                     
                     {loginForm()}
+
+                    <button
+                        onClick={loginWithGoogle}
+                        type="submit"
+                        className="w-full text-center py-3 rounded bg-red-500 text-white hover:bg-red-400 focus:outline-none my-1"
+                    >Log In with Google</button>
 
                 </div>
 
